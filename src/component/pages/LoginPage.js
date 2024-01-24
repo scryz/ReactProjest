7196d7f1b95476b41245c56dff258c071861978d
scryz <speretyagin2016@bk.ru>
2024-01-25 00:31:13 +0700

    NewAuth

--- src/component/pages/LoginPage.js
+++ src/component/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import React, { useState, useEffect }  from 'react';
 import axios from 'axios';
 import "../../css/login.css";
 import { jwtDecode } from 'jwt-decode';
@@ -6,15 +6,39 @@ import { jwtDecode } from 'jwt-decode';
 
 
 const LoginPage = () => {
- const [userName, setName] = useState('');
+ const [userName, setUserName] = useState('');
  const [password, setPassword] = useState('');
  const [errorMessage, setErrorMessage] = useState(null);
+ const [name, setName] = useState('');
+ const [age, setAge] = useState('');
 
+ useEffect(() => {
+  const signUpButton = document.getElementById('Зарегистрироваться');
+  const signInButton = document.getElementById('Войти');
+  const container = document.getElementById('container_log');
 
+  signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+  });
 
- const handleSubmit = async (e) => {
+  signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+  });
+
+  return () => {
+    signUpButton.removeEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
+
+    signInButton.removeEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
+  };
+}, []);
+
+ const handleSubmitLogin = async (e) => {
     e.preventDefault();
-
+    
     try {
       const response = await axios.post('https://localhost:7293/api/Auth/Login', {
           userName: userName,
@@ -33,9 +57,11 @@ const LoginPage = () => {
         localStorage.setItem('token', JSON.stringify(data));
         localStorage.setItem('token', token);
         
+        
 
 
       }
+      
       catch (err)
       {
         if (err.response) {
@@ -51,33 +77,88 @@ const LoginPage = () => {
        }
       }
     
-       
+
   
       }
+      const handleSubmitReg = async (e) => {
+        e.preventDefault();
+        
+        try {
+          const response = await fetch('https://localhost:7293/api/Auth/Register', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ userName, password, name, age }),
+          });
+          
+
+          setErrorMessage(null);
+
+    
+
+            const data = await response.json();
+            console.log(data);
+            setErrorMessage('Неверно введены данные или пропущены поля!');
+        } catch (error) {
+            if (error.response) {
+                if (error.response.status === 400) {
+                  setErrorMessage('Неправильно введены данные или пропущены поля!');
+                } else {
+                  setErrorMessage('Ошибка сервера');
+                }
+               } else {
+                setErrorMessage('Аккаунт успешно зарегистрирован!');
+                window.location.href="/";
+             }
+           
+        }
+    }
+
+
 
     
 
  return (
 
+<div class="container_log" id="container_log">
+	<div class="form-container_log sign-up-container_log">
+		<form action="#">
+			<h1>Создать аккаунт</h1>
+			<input type="text" name="userName" placeholder="Ник" value={userName} onChange={(e)=>setUserName(e.target.value)} />
+      <input type="text" name="name" placeholder="Имя" value={name} onChange={(e)=>setName(e.target.value)} />
+      <input type="number" name="age" placeholder="Возраст" value={age} onChange={(e)=>setAge(e.target.value)} />
+			<input type="password" name="name" placeholder="Пароль" value={password} onChange={(e)=>setPassword(e.target.value)} />
+			{errorMessage && <p>{errorMessage}</p>}
+      <button type="submit" onClick={handleSubmitReg}>Создать</button>
+		</form>
+	</div>
+	<div class="form-container_log sign-in-container_log">
+		<form action="#">
+			<h1>Авторизоваться</h1>
+			<input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="Ник"/>
+			<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Пароль"/>
+			<a href="#">Забыли пароль?</a>
+			{errorMessage && <p>{errorMessage}</p>}
+      <button type='submit' onClick={handleSubmitLogin}>Войти</button>
+		</form>
+	</div>
+	<div class="overlay-container_log">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Добро пожаловать!</h1>
+				<p>Уже есть аккаунт? Тогда жми кнопку ниже!</p>
+				<button class="ghost" id="Войти">Войти</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Привет!</h1>
+				<p>Ещё нет аккаунта? Тогда жми кнопку ниже!</p>
+				<button class="ghost" id="Зарегистрироваться">Зарегистрироваться</button>
+			</div>
+		</div>
+	</div>
+</div>
     
-    <main className="login_block">
-      <div className="logo">GetGether</div>
-      <div className="login_text">Войдите в аккаунт</div>
-      <div className="login">
-  
-        <div className="input-form">
-          <input type="text" value={userName} onChange={(e) => setName(e.target.value)} placeholder="Ник"/>
-        </div>
-        <div className="input-form">
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Пароль"/>
-        </div>
-      
-        {errorMessage && <p>{errorMessage}</p>}
-      <button type='submit' className="button" onClick={handleSubmit}>Войти</button>
-    
-      </div>
-    </main>
-
  );
 }
  
